de6b6d4597240379d22ebb177bfc1c4f84ae6fd5
oddflavour <kavkeon@gmail.com>
2020-09-03 13:04:48 -0400

    Added Navbar component

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -5,13 +5,15 @@ import {AppComponent} from './app.component';
 import {CalendarComponent} from './components/calendar/calendar.component';
 import {DayPlanComponent} from './components/day-plan/day-plan.component';
 import {CalendarCellComponent} from './components/calendar/calendar-cell/calendar-cell.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     CalendarComponent,
     DayPlanComponent,
-    CalendarCellComponent
+    CalendarCellComponent,
+    NavbarComponent
   ],
   imports: [
     BrowserModule
